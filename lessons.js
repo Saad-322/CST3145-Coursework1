1bd80deb77cbdde36f8b6f1ddf96f87375fd01d7
Saad-322 <saady6nov@gmail.com>
2021-10-28 11:45:03 +0100

    Display lessons using v-for and external JS file

---
+++ lessons.js
@@ -0,0 +1,11 @@
+let lessonsArray = [
+    { Image:"maths.jpg", Subject: "Maths", Location: "London", Price: "£100", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Maths", Location: "Oxford", Price: "£120", Spaces: 5},
+    { Image:"maths.jpg", Subject: "English", Location: "Oxford", Price: "£110", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Computer Science", Location: "Cambridge", Price: "£150", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Urdu", Location: "South Hampton", Price: "£90", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Spanish", Location: "London", Price: "£90", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Arabic", Location: "London", Price: "£80", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Chemistry", Location: "Manchester", Price: "£110", Spaces: 5},
+    { Image:"maths.jpg", Subject: "Physics", Location: "Huddersfield", Price: "£100", Spaces: 5},
+]
