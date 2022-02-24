2dd8a782c7c8d1322380dd5275879c21c4313e16
Saad-322 <saady6nov@gmail.com>
2022-02-24 12:23:24 +0000

    updated code

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,4 @@
 var cacheName = 'lessons_store';
-var cacheFiles = [
-  'index.html',
-  'lessons_store.webmanifest',
-  'images/arabic.jpeg',
-  'css/styles.css'
-];
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log('[Service Worker] Caching all the files');
-      return cache.addAll(cacheFiles);
-    }));
-});
-// self.addEventListener('fetch', function (e) {
-//   e.respondWith(
-//     // check if the cache has the file
-//     caches.match(e.request).then(function (r) {
-//       console.log('[Service Worker] Fetching resource: '
-//         + e.request.url);
-//       // 'r' is the matching file if it exists in the cache
-//       return r
-//     })
-//   );
-// });
 self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (r) {
